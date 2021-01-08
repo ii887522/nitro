6cf5dccdd0ec9abc6e1e22daa2a1d8f3b52c7744
Chi Min Yong <chimin.yong@g2g.com>
2021-01-08 23:59:53 +0800

    Add unary reactive

---
+++ nitro/src/test/Reactive/UnaryReactive.test.h
@@ -0,0 +1,12 @@
+#ifdef TEST
+
+#ifndef II887522_NITRO_UNARY_REACTIVE_TEST_H
+#define II887522_NITRO_UNARY_REACTIVE_TEST_H
+
+namespace ii887522::nitro
+{
+	void testUnaryReactive();
+}
+
+#endif
+#endif
