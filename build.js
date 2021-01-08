c9bdb44c55f855ecaf3f71f490957b85d3581b24
Chi Min Yong <chimin.yong@g2g.com>
2021-01-08 20:53:29 +0800

    Initial commit

---
+++ build.js
@@ -0,0 +1,9 @@
+'use strict'
+
+import { options, dependencies } from '@ii887522/packify'
+
+options.outDirPath = 'libs/'
+options.x86DllOutDirPaths = []
+options.x64DllOutDirPaths = []
+
+dependencies(() => { })
---
+++ nitro/src/test/test.cpp
@@ -0,0 +1,17 @@
+#ifdef TEST
+
+namespace ii887522::nitro
+{
+    static int main()
+    {
+
+        return 0;
+    }
+}
+
+int main()
+{
+    return ii887522::nitro::main();
+}
+
+#endif
