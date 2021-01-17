736f07eaa0c021f04c58b7cec00c762feb590e8e
Chi Min Yong <chimin.yong@g2g.com>
2021-01-17 11:02:03 +0800

    It only support Windows

--- install.sh
+++
@@ -1,3 +0,0 @@
-python -m pip install virtualenv
-virtualenv env
-source env/bin/activate && python -m pip install cpplint && source env/bin/deactivate && docker run --rm --name nitro_installer -w C:/nitro -v $PWD:C:/nitro nitro-custom-node ncu.cmd -u && npm.cmd install
