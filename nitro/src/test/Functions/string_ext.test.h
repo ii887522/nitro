e2e24a0147f3bc8df86c52f45b975a9e1d907bc3
Chi Min Yong <chimin.yong@g2g.com>
2021-05-15 12:08:52 +0800

    Add string functions

---
+++ nitro/src/test/Functions/string_ext.test.h
@@ -0,0 +1,15 @@
+// Copyright ii887522
+
+#ifdef TEST
+
+#ifndef NITRO_SRC_TEST_FUNCTIONS_STRING_EXT_TEST_H_
+#define NITRO_SRC_TEST_FUNCTIONS_STRING_EXT_TEST_H_
+
+namespace ii887522::nitro {
+
+void testStringExt();
+
+}  // namespace ii887522::nitro
+
+#endif
+#endif  // NITRO_SRC_TEST_FUNCTIONS_STRING_EXT_TEST_H_
